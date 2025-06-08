b180fd783bc21690deff5fd989ff116a9f873c94
Mohamad Sanas <159630755+MohamadSanas@users.noreply.github.com>
2025-06-08 18:08:32 +0530

    updated

--- Lab02/max_score.java
+++ Lab02/max_score.java
@@ -1,3 +1,4 @@
+import java.awt.im.InputContext;
 import java.util.Scanner;
 
 public class max_score {
@@ -8,7 +9,8 @@ public class max_score {
         System.out.println("Enter the number of questions and number of queries:");
         
         int N = input.nextInt();  // number of questions
-        int Q = input.nextInt();  // number of queries (not used yet)
+        int Q = input.nextInt();
+        int[] Question=new int [Q];   // number of queries (not used yet)
 
         // time to complete each question
         int[] T = new int[N];
@@ -22,18 +24,36 @@ public class max_score {
             S[i] = input.nextInt();
         }
 
-        // 0th row: score per minute, 1st row: question index
-        Double[][] scorPerMnt = new Double[2][N];
-        for (int i = 0; i < N; i++) {
-            scorPerMnt[0][i] = (double)S[i] / T[i];
-            scorPerMnt[1][i] = (double)i;
+        System.out.print("Enter the number of Questions");
+        for (int i = 0;i<Q; i++) {
+            Question[i]=input.nextInt();
         }
 
-        // sort and print
-        sort_array(scorPerMnt);
+        // 0th row: score per minute, 1st row: question index
+        int[][] mergArr = new int[2][N];
+        for (int i = 0; i < N; i++) {
+            mergArr[0][i] = S[i];
+            mergArr[1][i] = T[i];
+        }
+
+        int [][] SortedbyScore=sort_array(mergArr);
+        int totTime=0;
+        //Count total time taken for each queries
+
+
+        for (int j=0;j<Q;j++){
+            for(int i=0;i<Question[j];i++){
+            int index =  SortedbyScore[0][j];  // safer and more readable
+            totTime += T[index];
+            }
+            System.out.println(totTime);
+            totTime=0;
+        }
+        
+
     }
 
-    public static void sort_array(Double[][] numbers) {
+    public static int [][]  sort_array(int[][] numbers) {
         int N = numbers[0].length;
 
         // Bubble sort based on score per minute
@@ -41,12 +61,12 @@ public class max_score {
             for (int j = N - 1; j > i; j--) {
                 if (numbers[0][j] > numbers[0][j - 1]) {
                     // swap scores
-                    Double temp = numbers[0][j];
+                    int temp = numbers[0][j];
                     numbers[0][j] = numbers[0][j - 1];
                     numbers[0][j - 1] = temp;
 
                     // swap indices
-                    Double ind = numbers[1][j];
+                    int ind = numbers[1][j];
                     numbers[1][j] = numbers[1][j - 1];
                     numbers[1][j - 1] = ind;
                 }
@@ -54,9 +74,9 @@ public class max_score {
         }
 
         // print the sorted array
-        System.out.println("Sorted by score per minute (descending):");
         for (int i = 0; i < numbers[0].length; i++) {
             System.out.println("Question " + numbers[1][i] + ": " + numbers[0][i] + " pts/min");
         }
+        return numbers;
     }
 }
